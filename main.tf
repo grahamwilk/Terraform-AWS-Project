8cdf3dd1306835fd1e6a1e5743fe948c5b7af043
grahamwilk <bh5884is@go.minnstate.edu>
2025-05-29 02:42:19 -0500

    allow public access

--- main.tf
+++ main.tf
@@ -20,6 +20,15 @@ resource "aws_s3_bucket" "website_bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "website_bucket_public_access" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.website_bucket.id
 
