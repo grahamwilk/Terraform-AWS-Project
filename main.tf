65fed354c02fa6c53db83325c120ba25235ec50e
grahamwilk <bh5884is@go.minnstate.edu>
2025-05-29 02:35:22 -0500

    initial files test

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "my-demo-static-site-${random_id.suffix.hex}"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  tags = {
+    Name        = "Demo Static Website"
+    Environment = "Dev"
+  }
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+output "website_url" {
+  value = aws_s3_bucket.website_bucket.website_endpoint
+}
